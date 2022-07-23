4ebcaa6a6d35873c0c488bd510dd12014c5fc365
Emerson Millard <emillard.code@gmail.com>
2022-07-23 12:00:00 -0700

    Modified main class.

--- src/main/java/com/project/springcloudribbonjt2/SpringCloudRibbonJt2Application.java
+++ src/main/java/com/project/springcloudribbonjt2/SpringCloudRibbonJt2Application.java
@@ -2,12 +2,27 @@ package com.project.springcloudribbonjt2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
+@RibbonClient(name = "chatbook", configuration = RibbonConfiguration.class)
 public class SpringCloudRibbonJt2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudRibbonJt2Application.class, args);
+
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate template() {
+        return new RestTemplate();
     }
 
 }
