cbf09e34a105254340cc25684f1debafb2ecffe0
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Modified comments.

--- src/main/java/com/project/controller/RibbonController.java
+++ src/main/java/com/project/controller/RibbonController.java
@@ -1,16 +1,14 @@
 package com.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-// @RibbonClient(name = "chatbook", configuration = RibbonConfiguration.class)
 @RibbonClient(name = "chatbook")
+// To enable configuration class: @RibbonClient(name = "chatbook", configuration = RibbonConfiguration.class)
 public class RibbonController {
 
     @Autowired
