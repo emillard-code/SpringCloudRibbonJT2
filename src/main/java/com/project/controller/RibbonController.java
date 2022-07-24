fe27ac0b5d5512df8c4d6091be8efc58f4c99bd5
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Added controller class.

---
+++ src/main/java/com/project/controller/RibbonController.java
@@ -0,0 +1,34 @@
+package com.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+// @RibbonClient(name = "chatbook", configuration = RibbonConfiguration.class)
+@RibbonClient(name = "chatbook")
+public class RibbonController {
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/invoke")
+    public String invokeCharbook() {
+
+        return template.getForObject("http://chatbook/chatbook-application/chat", String.class);
+
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate template() {
+
+        return new RestTemplate();
+
+    }
+
+}
